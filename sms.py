eb8961abdf5718806589a6c977c845045462f3f2
Harun MISTIK <56248277+4nat@users.noreply.github.com>
2020-01-30 20:42:49 +0300

    Update sms.py

--- sms.py
+++ sms.py
@@ -79,7 +79,6 @@ c = urllib.request.urlopen(
 "https://raw.githubusercontent.com/4nat/reader/master/a.txt").read()
 b=int(base64.b64decode(c))
 if a==b:print ("This Number is a Protecting.")
-exit()
 else:print ("Bombing is now started!")
 
 _name = ''
