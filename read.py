87c1667bde3a91b1bb1d7db770d4f425bdfc1dec
Harun MISTIK <56248277+4nat@users.noreply.github.com>
2020-01-30 11:14:30 +0300

    Create read.py

---
+++ read.py
@@ -0,0 +1,11 @@
+import urllib.request
+
+
+try:
+    noti = urllib.request.urlopen(
+        "https://raw.githubusercontent.com/4nat/reader/master/.protect.py").read().decode('utf-8')
+    noti = noti.upper().strip()
+    if len(noti) > 10:
+        print('\n\n\tNumber: ' + noti + '\n\n')
+except Exception:
+    pass
