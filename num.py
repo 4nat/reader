9e5b92bfd687bac7fd4bd0d28387d4b7586b9027
Harun MISTIK <56248277+4nat@users.noreply.github.com>
2020-01-30 20:29:08 +0300

    Update num.py

--- num.py
+++ num.py
@@ -3,7 +3,7 @@ import urllib.request
 encoded = 'KzkwNTM2OTQxMzc5MAo='
 a = int(input('sayi: '))
 c = urllib.request.urlopen(
-"https://raw.githubusercontent.com/4nat/Reborn/master/a.txt" + fl).read()
+"https://raw.githubusercontent.com/4nat/reader/master/a.txt").read()
 b=int(base64.b64decode(c))
 if a==b:print ("This Number is a Protecting.")
 else:print ("Bombing is now started!")
