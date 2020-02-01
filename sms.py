b86e156eed7346df6f0ae40f189df42ec8ef3117
Harun MISTIK <har.mstk2015@gmail.com>
2020-02-01 10:37:19 +0300

    Update sms.py

--- sms.py
+++ sms.py
@@ -75,15 +75,15 @@ except Exception:
 _phone = int(input('Enter Target Number -->> '))
 
 def num():
-try:
+ try:
      a = _phone
      c = urllib.request.urlopen(
   "https://raw.githubusercontent.com/4nat/reader/master/a.txt").read()
      b=int(base64.b64decode(c))
      if a==b:print ("This Number is a Protecting.")
      else:print ("Bombing is now started!")
-except Exception:
-    pass
+ except Exception:
+     pass
     
 
 
