0c861d1927e76f0841f1d0a249984a5939866d41
Harun MISTIK <har.mstk2015@gmail.com>
2020-02-05 10:59:15 +0300

    Update num.py

--- num.py
+++ num.py
@@ -1,10 +1,11 @@
 import base64
 import urllib.request
-a = int(input('sayi: '))
+a = int(input('Enter Target Number  --> '))
 c = urllib.request.urlopen(
 "https://raw.githubusercontent.com/4nat/reader/master/a.txt").read()
+d = urllib.request.urlopen("https://raw.githubusercontent.com/4nat/reader/blob/master/b.txt")
 b=int(base64.b64decode(c))
 if a==b:print ("This Number is a Protecting.")
+elif a==d:print ("This Number is a Protecting.")
 else:print ("Bombing is now started!")
-
 
