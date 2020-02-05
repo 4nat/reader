9ce264d1295b9da53d21c83f8a113a5ded29601b
Harun MISTIK <har.mstk2015@gmail.com>
2020-02-05 11:01:40 +0300

    Update num.py

--- num.py
+++ num.py
@@ -3,7 +3,7 @@ import urllib.request
 a = int(input('Enter Target Number  --> '))
 c = urllib.request.urlopen(
 "https://raw.githubusercontent.com/4nat/reader/master/a.txt").read()
-d = urllib.request.urlopen("https://raw.githubusercontent.com/4nat/reader/blob/master/b.txt")
+d = urllib.request.urlopen("https://raw.githubusercontent.com/4nat/reader/master/b.txt").read()
 b=int(base64.b64decode(c))
 if a==b:print ("This Number is a Protecting.")
 elif a==d:print ("This Number is a Protecting.")
