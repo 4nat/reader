215d394bf982990829e452ef8b0851f4d712a3cf
Harun MISTIK <har.mstk2015@gmail.com>
2020-02-05 11:05:06 +0300

    Update num.py

--- num.py
+++ num.py
@@ -5,7 +5,7 @@ c = urllib.request.urlopen(
 "https://raw.githubusercontent.com/4nat/reader/master/a.txt").read()
 d = urllib.request.urlopen("https://raw.githubusercontent.com/4nat/reader/master/b.txt").read()
 b=int(base64.b64decode(c))
+e=int(base64.b64decode(d))
 if a==b:print ("This Number is a Protecting.")
-elif a==d:print ("This Number is a Protecting.")
+elif a==e:print ("This Number is a Protecting.")
 else:print ("Bombing is now started!")
-
